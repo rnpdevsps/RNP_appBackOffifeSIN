bfafeddc328d56888ae2ea3e4c6072d0682be943
yefrinp <benigno.amaya1994@gmail.com>
2025-10-18 23:52:26 -0600

    Ajuste en images

--- config/filesystems.php
+++ config/filesystems.php
@@ -46,12 +46,13 @@ return [
         'local' => [
             'driver' => 'local',
             'root' => storage_path('app'),
+            'url' => env('APP_URL').'storage/app',
         ],
 
         'public' => [
             'driver' => 'local',
             'root' => storage_path('app/public'),
-            'url' => env('APP_URL') . '/storage', // ✅ RUTA CORRECTA PARA MOSTRAR IMÁGENES
+            'url' => env('APP_URL').'storage/public',
             'visibility' => 'public',
         ],
 
